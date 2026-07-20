5b5fcdd4bd3116830c6f50f44768f8566ba15997
mikasa-girly <fitia.solofonirina@gmail.com>
2026-07-20 09:44:56 +0300

    basese

--- base.sql
+++ base.sql
@@ -40,3 +40,68 @@ CREATE TABLE vente (
     quantite INT,
     FOREIGN KEY (id_produit_membre) REFERENCES produit_membre(id_produit_membre)
 );
+
+
+INSERT INTO membre (nom, numero_etu, image_profil) VALUES
+('Miora', 'etu004695', 'miora.jpg'),
+('Fitia', 'etu004696', 'fitia.jpg'),
+('Lucas', 'etu004702', 'lucas.jpg'),
+('Sedra', 'etu004703', 'sedra.jpg'),
+('Johanna', 'etu004697', 'johanna.jpg'),
+('Johnattan', 'etu004698', 'johnattan.jpg'),
+('Raissa', 'etu004699', 'raissa.jpg'),
+('Mendrika', 'etu004700', 'mendrika.jpg'),
+('Tsiky', 'etu004701', 'tsiky.jpg'),
+('Harena', 'etu004704', 'harena.jpg');
+
+INSERT INTO categorie (nom_categorie) VALUES
+('plat'),
+('boisson'),
+('snack'),
+('dessert');
+
+
+INSERT INTO produit (nom, id_categorie, prix_reference) VALUES
+
+('Riz au poulet', 1, 4500.00),
+('Sauce zebut', 1, 3500.00),
+('Lasary', 1, 1500.00),
+('Koba', 1, 2000.00),
+
+('Coca 33cl', 2, 2000.00),
+('Eau 50cl', 2, 1000.00),
+('Jus ananas', 2, 2500.00),
+('The glacé', 2, 2200.00),
+
+('Sambos boeuf', 3, 800.00),
+('Nem poulet', 3, 1000.00),
+('Chips', 3, 1500.00),
+('Gateau sec', 3, 500.00),
+
+('Yaourt', 4, 1800.00),
+('Salade de fruits', 4, 3000.00),
+('Mousse au chocolat', 4, 3500.00);
+
+INSERT INTO produit_membre (id_produit, id_membre, prix_vente, quantite_dispo, date_dispo) VALUES
+(1, 1, 5000.00, 5, '2026-04-24'), 
+(2, 1, 4000.00, 3, '2026-04-24'),  
+(5, 2, 2200.00, 10, '2026-04-24'), 
+(6, 2, 1200.00, 8, '2026-04-24'),  
+(9, 3, 900.00, 15, '2026-04-24'), 
+(10, 3, 1100.00, 12, '2026-04-24'),
+(13, 4, 2000.00, 6, '2026-04-24'), 
+(15, 4, 3800.00, 4, '2026-04-24'), 
+(3, 5, 1700.00, 7, '2026-04-24'),  
+(7, 5, 2700.00, 5, '2026-04-24'),  
+(11, 6, 1700.00, 9, '2026-04-24'), 
+(12, 6, 600.00, 20, '2026-04-24'), 
+(4, 7, 2200.00, 10, '2026-04-24'), 
+(8, 7, 2400.00, 6, '2026-04-24'),  
+(14, 8, 3200.00, 3, '2026-04-24'), 
+(1, 8, 4800.00, 2, '2026-04-24'),  
+(5, 9, 2100.00, 5, '2026-04-24'),  
+(9, 9, 850.00, 8, '2026-04-24'),   
+(13, 10, 1900.00, 4, '2026-04-24'),
+(2, 10, 3800.00, 2, '2026-04-24'); 
+
+
