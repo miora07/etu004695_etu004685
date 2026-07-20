ac69c3bed83d46cdac2d53631db9784620aaebb6
mikasa-girly <fitia.solofonirina@gmail.com>
2026-07-20 10:00:25 +0300

    tache,index,acceuil

--- functions.php
+++ functions.php
@@ -24,46 +24,13 @@ function checkLogin($email, $password)
     $sql = sprintf($sql, $email, $password);
     return get_one_line($sql);
 }
-function get_random_songs($limit, $user_id)
-{
-    $sql = "SELECT 
-    s.id,
-    s.title,
-    ar.name AS artiste,
-    al.title AS album,
-    SEC_TO_TIME(s.duration) AS duree,
-    (l.song_id IS NOT NULL) AS liked
-    FROM songs s
-    JOIN artists ar ON s.artist_id = ar.id
-    JOIN albums al ON s.album_id = al.id
-    LEFT JOIN likes l 
-        ON l.song_id = s.id 
-        AND l.user_id = %d ORDER BY RAND() LIMIT %d";
-    $sql = sprintf($sql, $user_id, $limit);
-    //echo $sql;
-    return get_all_lines($sql);
+
+
+
+
+function get_all_produit() {
+    $sql = "SELECT * from produits";
+    retun get_all_lines($sql); 
 }
 
-function manage_favorite($user_id, $song_id, $action)
-{
-    if ($action === 'like') {
-        set_favorite($user_id, $song_id);
-    } elseif ($action === 'unlike') {
-        unset_favorite($user_id, $song_id);
-    }
-}
-
-function set_favorite($user_id, $song_id)
-{
-    $sql = "INSERT INTO likes (user_id, song_id) VALUES (%d, %d)";
-    $sql = sprintf($sql, $user_id, $song_id);
-    mysqli_query(dbconnect(), $sql);
-}
-
-function unset_favorite($user_id, $song_id)
-{
-    $sql = "DELETE FROM likes WHERE user_id=%d AND song_id=%d";
-    $sql = sprintf($sql, $user_id, $song_id);
-    mysqli_query(dbconnect(), $sql);
-}
 ?>
---
+++ tache.md
@@ -0,0 +1,9 @@
+base.sql:
+    -creation base de donnnes
+    -creation table 
+    -insertion des donnees
+functions.php:  
+    -creation fonction pour se connecter
+    -creation fonction pour afficher tous les produits 
+connection.php:
+    -creation d'une fonction pour se connecter au base de donnees 
